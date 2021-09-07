17762c9791037ac9ca36e8dfcc7e5d693b5d3621
amkwenene <amkwenene@gmail.com>
2021-09-06 23:51:11 -0700

    last push

--- src/test/java/runner/apiRunner.java
+++ src/test/java/runner/apiRunner.java
@@ -6,9 +6,9 @@ import org.junit.runner.RunWith;
 import org.testng.annotations.BeforeClass;
 
 
-public class webRunner {
+public class apiRunner {
     @RunWith(Cucumber.class)
-    @CucumberOptions(features = {"src/main/resource/feature/web"},
+    @CucumberOptions(features = {"src/main/resource/feature/api"},
             glue = {"stepdefination"},
             plugin = {"com.cucumber.listener.ExtentCucumberFormatter:","pretty","html:target/cucumber-html-report","json:target/cucumber.json"},
             //plugin={"pretty:target/cucumber-htmlreport.text", "json:target/cucmber-report.json"},
